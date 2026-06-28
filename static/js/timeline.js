f3031b7706e881a7b6765c747e4c3fbead4f4d4a
4秒的热水袋 <Albert_ZhuJingChen@126.com>
2026-06-28 11:58:01 +0800

    Update timeline.js

--- static/js/timeline.js
+++ static/js/timeline.js
@@ -5,3 +5,11 @@ function openModal() {
 function closeModal() {
     document.getElementById('modal-overlay').classList.remove('show');
 }
+
+function openModal2() {
+    document.getElementById('modal-overlay-2').classList.add('show');
+}
+
+function closeModal2() {
+    document.getElementById('modal-overlay-2').classList.remove('show');
+}
