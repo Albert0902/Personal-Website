711e283ac033f0973ff34e07c8ee729b9e3bba42
4秒的热水袋 <Albert_ZhuJingChen@126.com>
2026-07-19 19:43:56 +0800

    Add files via upload

---
+++ static/js/timeline.js
@@ -0,0 +1,23 @@
+function openModal() {
+    document.getElementById('modal-overlay').classList.add('show');
+}
+
+function closeModal() {
+    document.getElementById('modal-overlay').classList.remove('show');
+}
+
+function openModal2() {
+    document.getElementById('modal-overlay-2').classList.add('show');
+}
+
+function closeModal2() {
+    document.getElementById('modal-overlay-2').classList.remove('show');
+}
+
+function openModal3() {
+    document.getElementById('modal-overlay-3').classList.add('show');
+}
+
+function closeModal3() {
+    document.getElementById('modal-overlay-3').classList.remove('show');
+}
