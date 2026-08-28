7978b70396b09096e449e281dac2daeabc1c2736
4秒的热水袋 <Albert_ZhuJingChen@126.com>
2026-08-28 21:29:14 +0800

    Update timeline.js

--- static/js/timeline.js
+++ static/js/timeline.js
@@ -1,31 +1,7 @@
-function openModal() {
-    document.getElementById('modal-overlay').classList.add('show');
+function openPopup(id) {
+    document.getElementById(id).classList.add('show');
 }
 
-function closeModal() {
-    document.getElementById('modal-overlay').classList.remove('show');
-}
-
-function openModal2() {
-    document.getElementById('modal-overlay-2').classList.add('show');
-}
-
-function closeModal2() {
-    document.getElementById('modal-overlay-2').classList.remove('show');
-}
-
-function openModal3() {
-    document.getElementById('modal-overlay-3').classList.add('show');
-}
-
-function closeModal3() {
-    document.getElementById('modal-overlay-3').classList.remove('show');
-}
-
-function openModal4() {
-    document.getElementById('modal-overlay-4').classList.add('show');
-}
-
-function closeModal4() {
-    document.getElementById('modal-overlay-4').classList.remove('show');
+function closePopup(id) {
+    document.getElementById(id).classList.remove('show');
 }
