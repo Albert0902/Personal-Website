7284bedc7d8bbe560da4fee4bb9a8770aab07b28
4秒的热水袋 <Albert_ZhuJingChen@126.com>
2026-09-24 18:39:05 +0800

    Delete static directory

--- static/js/qr.js
+++
@@ -1,7 +0,0 @@
-function openQrPopup() {
-    document.getElementById('qr-popup').classList.add('show');
-}
-
-function closeQrPopup() {
-    document.getElementById('qr-popup').classList.remove('show');
-}
--- static/js/timeline.js
+++
@@ -1,7 +0,0 @@
-function openPopup(id) {
-    document.getElementById(id).classList.add('show');
-}
-
-function closePopup(id) {
-    document.getElementById(id).classList.remove('show');
-}
